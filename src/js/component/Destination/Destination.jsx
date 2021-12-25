58bfce8f87b5a2755af87847e8c4f12a2d9845c7
sylla ibrahim <s67986601@gmail.com>
2021-12-25 03:17:37 +0000

    destination

--- src/js/component/Destination/Destination.jsx
+++ src/js/component/Destination/Destination.jsx
@@ -1,40 +1,86 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import data from "../../../assets/data.json";
+import "./destination.scss";
+import mars from "../../../assets/destination/image-mars.png"
+import moon from "../../../assets/destination/image-moon.png"
+import titan from "../../../assets/destination/image-titan.png"
+import europa from "../../../assets/destination/image-europa.png"
+
+
+
 export default class Destination extends Component {
+  constructor(props) {
+    super(props);
+    this.hanbleChange = this.hanbleChange.bind(this);
+    this.state = { focus: 0 ,img:moon};
+  }
 
-    constructor(props){
-        super(props);
+  hanbleChange(e) {
+    let type = e.target.innerText;
+    if (type === "Moon") {
+      this.setState({ focus: 0,img:moon});
+    } else if (type === "Mars") {
+      this.setState({ focus: 1 ,img:mars});
+    } else if (type === "Europa") {
+      this.setState({ focus: 2 ,img:europa});
+    } else if (type === "Titan") {
+      this.setState({ focus: 3 ,img:titan});
     }
+  }
 
-    render(){
-        return(
-            <div className="destination">
-                <h3 className="destination__title">{data.destinations[0].name}</h3>
-                <div className="destination__card">
-                    <div className="destination__card--img">
-                        <img src="../../../assets/destination/background-destination-desktop.jpg" alt="moon"/>
-                    </div>
-                    <div className="destination__card--desc">
-                        <nav className="destination__card--desc--nav">
-                            <p className="destination__card--desc--nav__element">{data.destinations[0].name}</p>
-                            <p className="destination__card--desc--nav__element">{data.destinations[1].name}</p>
-                            <p className="destination__card--desc--nav__element">{data.destinations[2].name}</p>
-                            <p className="destination__card--desc--nav__element">{data.destinations[3].name}</p>
-                        </nav>
-                        <h1 className="destination__card--desc--title">
-                        {data.destinations[0].name}
-                        </h1>
-                        <p className="destination__card--desc--details">
-                            {data.destinations[0].description}
-                        </p>
-                        <div className="destination__card--desc--info">
-                        <div className="destination__card--desc--info__distance">{data.destinations[0].distance}</div>
-                        <div className="destination__card--desc--info__travel">{data.destinations[0].travel}</div>
-                        </div>
-                </div>
-                </div>
+  render() {
+    return (
+      <div className="destination">
+        <h3 className="destination__title">
+          <span>01</span> Pick your destination
+        </h3>
+        <div className="destination__card">
+          <img className="destination__card--img" src={this.state.img} />
+          <div className="destination__card--desc">
+            <nav className="destination__card--desc--nav">
+              <p
+                className="destination__card--desc--nav__element"
+                onClick={this.hanbleChange}
+              >
+                {data.destinations[0].name}
+              </p>
+              <p
+                className="destination__card--desc--nav__element"
+                onClick={this.hanbleChange}
+              >
+                {data.destinations[1].name}
+              </p>
+              <p
+                className="destination__card--desc--nav__element"
+                onClick={this.hanbleChange}
+              >
+                {data.destinations[2].name}
+              </p>
+              <p
+                className="destination__card--desc--nav__element"
+                onClick={this.hanbleChange}
+              >
+                {data.destinations[3].name}
+              </p>
+            </nav>
+            <h1 className="destination__card--desc--title">
+              {data.destinations[this.state.focus].name}
+            </h1>
+            <p className="destination__card--desc--details">
+              {data.destinations[this.state.focus].description}
+            </p>
+            <p className="destination__card--desc--hr"></p>
+            <div className="destination__card--desc--info">
+              <div className="destination__card--desc--info__distance">
+                <p>AVG. DISTANCE</p> {data.destinations[this.state.focus].distance}
+              </div>
+              <div className="destination__card--desc--info__travel">
+                <p>Est. travel time</p> {data.destinations[this.state.focus].travel}
+              </div>
             </div>
-        );
-    }
-    
-};
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
